1883cf4b8e5d09c015e18a292477fc11c5598ade
gabriel prado <analise.dados.biel@gmail.com>
2024-03-05 20:41:28 -0300

    js

---
+++ index.js
@@ -0,0 +1,8 @@
+let email = prompt('digite seu email corretamente: ')
+
+while(email != 'gabriel@gmail.com'){
+    console.log(`O email digitado foi: ${email}`)
+
+
+    email = prompt('digite seu email corretamente: ')
+}
