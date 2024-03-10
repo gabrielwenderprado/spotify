4713eb9c8a4373e7d8f8b60e67bab52e7674df93
gabriel prado <analise.dados.biel@gmail.com>
2024-03-10 17:13:19 -0300

    aat e adc page entrar

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-let email = prompt('digite seu email corretamente: ')
+// let email = prompt('digite seu email corretamente: ')
 
-while(email != 'gabriel@gmail.com'){
-    console.log(`O email digitado foi: ${email}`)
+// while(email != 'gabriel@gmail.com'){
+//     console.log(`O email digitado foi: ${email}`)
 
 
-    email = prompt('digite seu email corretamente: ')
+//     email = prompt('digite seu email corretamente: ')
+// }
+
+function xicara(c, f, p, cp) {
+    this.cor = c;
+    this.forma = f;
+    this.peso = p;
+    this.capacidade = cp;
 }
+
+const xicara1 = new xicara("azul", "redonda", 25, 100)
+
+console.log(xicara1)
