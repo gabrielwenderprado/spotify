d0ea99e853b6647670ba0085eac405b87200bc4b
gabriel prado <analise.dados.biel@gmail.com>
2024-03-11 18:30:05 -0300

    add js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// let email = prompt('digite seu email corretamente: ')
+let email = prompt('digite seu email corretamente: ')
 
-// while(email != 'gabriel@gmail.com'){
-//     console.log(`O email digitado foi: ${email}`)
+while(email != 'gabriel-wender.prado@gmail.com'){
+    console.log(`O email digitado foi: ${email}`)
 
 
-//     email = prompt('digite seu email corretamente: ')
-// }
-
-function xicara(c, f, p, cp) {
-    this.cor = c;
-    this.forma = f;
-    this.peso = p;
-    this.capacidade = cp;
+    email = prompt('digite seu email corretamente: ')
 }
 
-const xicara1 = new xicara("azul", "redonda", 25, 100)
+function coletaDados(e, t, n, sn) {
+    this.email = e;
+    this.tipo = t;
+    this.nome = n;
+    this.sobreNome = sn;
+}
 
-console.log(xicara1)
+const coletaDados1 = new coletaDados("gabriel-wender.prado", "@gmail.com", "gabriel", "wender")
+
+console.log(coletaDados1)
