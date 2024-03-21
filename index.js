f9f91a47ff46be251267e8547ed41842820af5cd
gabriel prado <analise.dados.biel@gmail.com>
2024-03-20 22:28:54 -0300

    att js , acrecentando html e css

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-let email = prompt('digite seu email corretamente: ')
+ 
 
-while(email != 'gabriel-wender.prado@gmail.com'){
-    console.log(`O email digitado foi: ${email}`)
+    const login = document.getElementById("login")
+    const senha = document.getElementById("senha")
 
+   
+    const Dados = [];
 
-    email = prompt('digite seu email corretamente: ')
-}
-
-class dadosInscrevase {
-    constructor (email , nome , sobreNome, idade) {
-        this.email = email;
-        this.nome = nome;
-        this.sobreNome = sobreNome;
-        this.idade = idade
+    function SalvarInfosLogar () {
+        Dados.push ({
+            login: login.value,
+            senha: senha.value
+        })
+        if(login.value == "gabriel@gmail.com" && senha.value == "gabriel"){
+            alert('Olá, Gabriel');
+            location.href = "posEntrar.html";
+        } else{
+            alert('Usuario ou senha incorretos');
+        }
+    
+        console.log(Dados)
     }
-    apresentar= () => {
-        console.log ( `Ola meu Email É ${this.email} , Meu nome é ${this.nome} ${this.sobreNome}, E a minha idade é ${this.idade} `)
-    }
-}
- const dadosInscrevasenew = new dadosInscrevase ("gabriel@gmail.com", "gabriel","wender", 28)
-
- dadosInscrevasenew.apresentar()
+ 
