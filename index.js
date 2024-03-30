8c7b79382af6137237cd48ccbc80934f75a5fd2c
gabriel prado <analise.dados.biel@gmail.com>
2024-03-30 14:56:32 -0300

    att js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
     const login = document.getElementById("login")
     const senha = document.getElementById("senha")
 
+    const senha1 = document.querySelector('#senha');
+    const confirma = document.querySelector('#senhaConfirma');
+
    
     const Dados = [];
 
@@ -18,9 +21,82 @@
             alert('Usuario ou senha incorretos');
         }
     
-        console.log(Dados)
+        console.log(...Dados)
     }
- 
+
+    
+        //testando conferer senha 
+        const input1 = document.querySelector("#password")
+        const input2 = document.querySelector("#confirmPassword")
+    
+        input1.addEventListener("change", (event1)=> {
+        input2.addEventListener("change", (event2)=> {
+            if(event1.target.value === event2.target.value){
+                alert("Senhas iguais")
+            }else{
+                alert("Senhas não conferem")
+            }
+        })
+        })
+    
+        //testando validaçao de senha 
+            const senhaImpar = document.querySelector("#password")
+            const senhaPar = document.querySelector("#confirmPassword")
+            const button = document.querySelector("#button")
+            
+            
+            function valida(){
+            if(senhaImpar.value === senhaPar.value){
+                console.log("Senhas iguais")
+            }else{
+                console.log("Senhas não conferem")
+            }
+            }
+            button.addEventListener("click", valida)
+    
+            input1.addEventListener("change", (event1)=> {
+            input2.addEventListener("change", (event2)=> {
+                if(event1.target.value === event2.target.value){
+                console.log("Senhas iguais")
+                }else {
+                console.log("Senhas não conferem")
+                }
+            })
+            })
+
+            
+
+
+
+
+
+            
+    let primeiraSenha = ""
+    senha1.addEventListener('change', (e) => primeiraSenha = e.target.value)
+    console.log(primeiraSenha)
+    
+    function confereSenha(){
+        
+        console.log()
+        if (confirma.value === senha1.value) {
+            confirma.setCustomValidity('');
+        } else {
+            confirma.setCustomValidity('As senhas nao conferem');
+        }
+        
+    }
+
+    function senhaOk() {
+        alert("senhas conferem")
+    }
+
+
+
+
+
+
+
+
 
 
     const idade = {
@@ -42,15 +118,9 @@
 
 
 
-    const image = document.querySelector('conteudo');
 
-    image.addEventListener('click', (event) => {
-    const {} = event.target;
-    alert(`Atributo de origem da imagem: "${pagode.html}".`);
 
-    // Redirecionar para outra página:
-    location.replace(pagode.html);
-});
 
+    
     
 
